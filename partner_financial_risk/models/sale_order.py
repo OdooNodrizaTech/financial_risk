4f888f6ca7968aa3d1d5b79074bfcb5cc2ca6777
Informatica Arelux <informatica@arelux.com>
2020-04-02 11:39:24 +0200

    fix

--- partner_financial_risk/models/sale_order.py
+++ partner_financial_risk/models/sale_order.py
@@ -44,3 +44,17 @@ class SaleOrder(models.Model):
             if len(account_payment_mode_ids_need_check_credit_limit)>0:
                 if self.payment_mode_id.id in account_payment_mode_ids_need_check_credit_limit:                    
                     self.need_check_credit_limit = True
+                    
+    @api.multi
+    def action_confirm(self):
+        #check
+        allow_confirm = True
+        for obj in self:            
+            if obj.need_check_credit_limit==True:
+                future_max_credit_limit_allow = obj.max_credit_limit_allow - obj.amount_total
+                if future_max_credit_limit_allow<=0:
+                    allow_confirm = False
+                    raise Warning("No se puede confirmar la venta porque no hay credito disponible o el importe total de esta venta es superior al credito disponible ("+str(future_max_credit_limit_allow)+")")        
+        #allow_confirm
+        if allow_confirm==True:
+            return super(SaleOrder, self).action_confirm()                    
