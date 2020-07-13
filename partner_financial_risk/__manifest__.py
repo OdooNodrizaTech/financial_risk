9af64b1b4729e713b87338fd700d39bd8311a287
victoralmau <victoralmau@gmail.com>
2020-07-13 09:48:46 +0200

    fix

--- partner_financial_risk/__manifest__.py
+++ partner_financial_risk/__manifest__.py
@@ -1,18 +1,24 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
-    'name': 'Partner financial risk',
-    'version': '12.0.1.0.0',    
-    'author': 'Odoo Nodriza Tech (ONT)',
-    'website': 'https://nodrizatech.com/',
-    'category': 'Tools',
-    'license': 'AGPL-3',
-    'depends': ['base', 'account', 'sale', 'crm'],
-    'data': [
-        'views/account_payment_mode_view.xml',
-        'views/crm_lead_view.xml',
-        'views/res_partner_view.xml',
-        'views/sale_order_view.xml',
+    "name": "Partner financial risk",
+    "version": "12.0.1.0.0",
+    "category": "Sales Management",
+    "website": "https://nodrizatech.com/",
+    "author": "Odoo Nodriza Tech (ONT), "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "account",
+        "sale",
+        "crm"
     ],
-    'installable': True,
-    'auto_install': False,    
+    "data": [
+        "views/account_payment_mode_view.xml",
+        "views/crm_lead_view.xml",
+        "views/res_partner_view.xml",
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
 }
