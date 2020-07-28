0038352ce4c1399fa742651b51884ccb4f257d2d
victoralmau <victoralmau@gmail.com>
2020-07-28 14:51:34 +0200

    Update __manifest__.py

--- partner_financial_risk/__manifest__.py
+++ partner_financial_risk/__manifest__.py
@@ -13,7 +13,9 @@
         "account",
         "sale",
         "crm",
-        "account_payment_mode"  # https://github.com/OCA/bank-payment
+        "account_payment_mode",  # https://github.com/OCA/bank-payment
+        "account_payment_partner",  # https://github.com/OCA/bank-payment
+        "account_payment_sale"  # https://github.com/OCA/bank-payment
     ],
     "data": [
         "views/account_payment_mode_view.xml",
